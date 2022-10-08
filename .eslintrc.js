ede44e8e915a8d688f1c5a734a526e077b2275f6
cjh <1964646121@qq.com>
2022-10-08 18:59:27 +0800

    eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-essential",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  overrides: [
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true // 允许代码中使用 jsx
+    }
+  },
+  plugins: [
+    "vue",
+    "@typescript-eslint"
+  ],
+  rules: {
+  }
+}
